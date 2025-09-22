e2eaaa9a6d9357a7011764e3130b53f95cb22020
Ross Oliver <roliver@adobe.com>
2025-09-22 13:55:45 -0700

    Update accordion .js

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -4,6 +4,8 @@
  * https://www.hlx.live/developer/block-collection/accordion
  */
 
+import { moveInstrumentation } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
   [...block.children].forEach((row) => {
     // decorate accordion item label
